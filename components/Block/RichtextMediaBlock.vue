17b9f086d7f56050f7e53adf86e45982937cc8b8
Killian Habasque <khabasque@doowup.fr>
2023-11-08 08:06:35 +0100

    feat RichtextMedia block

--- components/Block/RichtextMediaBlock.vue
+++ components/Block/RichtextMediaBlock.vue
@@ -1,8 +1,11 @@
 <script setup>
 defineProps(['component'])
-
+import RichtextBlock from '@/components/Block/RichtextBlock'
+import MediaBlock from '@/components/Block/MediaBlock'
 </script>
 
 <template>
+    <MediaBlock :component="component.mediaComponent[0]"/>
+    <RichtextBlock :component="component.richtextComponent[0]"/>
     <p>{{ component.layout[0] }}</p>
 </template>
